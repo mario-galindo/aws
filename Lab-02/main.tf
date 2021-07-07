ad04fcf5da0afd89131813f2196bf5287f94165a
mario-galindo <mgalindo9410@gmail.com>
2021-07-07 17:23:33 -0600

    Base templeta to deploy basic network infrastructure

---
+++ Lab-02/main.tf
@@ -0,0 +1,142 @@
+############## 
+# VARIABLES
+##############
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "key_name" {}
+variable "private_key_path" {}
+variable "environment_tag" {}
+variable "region" {
+  default = "us-east-2"
+}
+variable "network_address_space" {
+  default = "10.1.0.0/16"
+}
+variable "subnet1_address_space" {
+  default = "10.1.0.0/24"
+}
+
+##############
+# PROVIDERS
+##############
+
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.region
+}
+
+##############
+# LOCALS
+##############
+
+locals {
+  common_tags = {
+    Environment = var.environment_tag
+  }
+}
+
+##############
+# DATA
+##############
+
+data "aws_availability_zones" "availabe" {}
+
+##############
+# RESOURCES
+##############
+
+# Networking #
+
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.network_address_space
+  enable_dns_hostnames = "true"
+}
+
+resource "aws_subnet" "subnet1" {
+  cidr_block              = var.subnet1_address_space
+  vpc_id                  = aws_vpc.vpc.id
+  map_public_ip_on_launch = "true"
+  availability_zone       = data.aws_availability_zones.availabe.name[0]
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+# Routing #
+resource "aws_route_table" "rtb" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "rta_subnet1" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.rtb.id
+}
+
+# Security Groups #
+resource "aws_security_group" "nginx-sg" {
+
+  name        = "nginx_sg"
+  description = "Allow ports for nginx server"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "nginx1" {
+
+  ami                    = "ami-04f2aebe9a95f0968"
+  instance_type          = "t2.micro"
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.nginx-sg.id]
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ec2-user"
+    private_key = file(var.private_key_path)
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install nginx -y",
+      "sudo service nginx start",
+      "sudo rm /usr/share/nginx/html/index.html",
+      "echo '<html><head><title>Blue Team Server</title></head><body style=\"background-color:#1F778D\"><p style=\"text-align: center;\"><span style=\"color:#FFFFFF;\"><span style=\"font-size:28px;\">Blue Team</span></span></p></body></html>' | sudo tee /usr/share/nginx/html/index.html"
+    ]
+  }
+}
+
+##############
+# OUTPUT
+##############
+
+output "aws_instance_public_dns" {
+  value = aws_instance.nginx1.public_dns
+}
